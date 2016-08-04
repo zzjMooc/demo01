afeefe64589ab9fc6ac6c88b380a0b78de9b2069
zhangzhijie14789 <zhangzhijie547@163.com>
2016-08-04 13:06:11 +0800

    2016/8/4

---
+++ js/Js_1.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+$(document).ready(function(){
+	$("#btnJSON").bind("click",function(){
+		
+	})
+})
+function getJSON(){
+	$.ajax({
+		type:"post",
+		url:"/Demo07/JsonAction.action",
+		dataType:"json",
+		
+		success:function(data){
+			var json=eval(json);
+			console.log(json);
+		}
+	});
+}
